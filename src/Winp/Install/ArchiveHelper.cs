6790b9b5bb8cb95c28873aa20b416acf8e80465b
Remi Caput <979446+r3c@users.noreply.github.com>
2023-01-22 09:10:11 +0100

    Run CI with dotnet 7.0.x

--- src/Winp/Install/ArchiveHelper.cs
+++ src/Winp/Install/ArchiveHelper.cs
@@ -1,67 +1,76 @@
 using System;
 using System.IO;
 using System.IO.Compression;
-using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Winp.Install
 {
     internal static class ArchiveHelper
     {
-        private const string UserAgent =
-            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
-
         public static async Task<string?> DownloadAndExtract(Uri archiveSource, string archivePath, Uri destination)
         {
-            using (var client = new WebClient())
+            using var client = new HttpClient();
+
+            HttpResponseMessage response;
+
+            try
             {
-                byte[] data;
-
-                client.Headers.Add(HttpRequestHeader.UserAgent, UserAgent);
-
-                try
+                response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, archiveSource)
                 {
-                    data = await client.DownloadDataTaskAsync(archiveSource);
-                }
-                catch (WebException exception)
-                {
-                    return exception.Message;
-                }
-
-                await using var stream = new MemoryStream(data);
-
-                try
-                {
-                    using var archive = new ZipArchive(stream, ZipArchiveMode.Read);
-
-                    var prefix = string.IsNullOrEmpty(archivePath) ? string.Empty : archivePath.TrimEnd('/') + "/";
-
-                    foreach (var entry in archive.Entries)
+                    Headers =
                     {
-                        if (entry.FullName.Length <= prefix.Length || !entry.FullName.StartsWith(prefix))
-                            continue;
-
-                        var sourcePathRelative = entry.FullName.Substring(prefix.Length);
-                        var destinationPath = Path.Combine(destination.AbsolutePath, sourcePathRelative);
-
-                        if (entry.Length == 0)
-                            Directory.CreateDirectory(destinationPath);
-                        else
+                        UserAgent =
                         {
-                            Directory.CreateDirectory(Path.GetDirectoryName(destinationPath) ?? string.Empty);
-
-                            entry.ExtractToFile(destinationPath, true);
+                            new ProductInfoHeaderValue("Mozilla", "5.0"),
+                            new ProductInfoHeaderValue("(Windows NT 6.1; WOW64)"),
+                            new ProductInfoHeaderValue("AppleWebKit", "535.1"),
+                            new ProductInfoHeaderValue("(KHTML, like Gecko)"),
+                            new ProductInfoHeaderValue("Chrome", "13.0.782.112"),
+                            new ProductInfoHeaderValue("Safari", "535.1")
                         }
                     }
-                }
-                catch (InvalidDataException exception)
+                });
+            }
+            catch (HttpRequestException exception)
+            {
+                return exception.Message;
+            }
+
+            await using var stream = await response.Content.ReadAsStreamAsync();
+
+            try
+            {
+                using var archive = new ZipArchive(stream, ZipArchiveMode.Read);
+
+                var prefix = string.IsNullOrEmpty(archivePath) ? string.Empty : archivePath.TrimEnd('/') + "/";
+
+                foreach (var entry in archive.Entries)
                 {
-                    return exception.Message;
+                    if (entry.FullName.Length <= prefix.Length || !entry.FullName.StartsWith(prefix))
+                        continue;
+
+                    var sourcePathRelative = entry.FullName.Substring(prefix.Length);
+                    var destinationPath = Path.Combine(destination.AbsolutePath, sourcePathRelative);
+
+                    if (entry.Length == 0)
+                        Directory.CreateDirectory(destinationPath);
+                    else
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(destinationPath) ?? string.Empty);
+
+                        entry.ExtractToFile(destinationPath, true);
+                    }
                 }
-                catch (IOException exception)
-                {
-                    return exception.Message;
-                }
+            }
+            catch (InvalidDataException exception)
+            {
+                return exception.Message;
+            }
+            catch (IOException exception)
+            {
+                return exception.Message;
             }
 
             return null;
