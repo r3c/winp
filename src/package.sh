94937e381d29764f9d23b6422ed5c2a8756e5964
Remi Caput <v.mirari.fr+git@mirari.fr>
2026-03-29 12:42:16 +0200

    Remove version number from packaged archive.

--- src/package.sh
+++ src/package.sh
@@ -5,23 +5,12 @@ artifact=winp
 project=Winp
 runtimes='win-x64'
 
-# Retreive latest version tag from current HEAD
-base="$(dirname "$0")"
-tag="$(git --work-tree "$base" tag --points-at HEAD)"
-
-if [ -n "$tag" ]; then
-	version="v$tag"
-else
-	echo >&2 "warning: missing tag on HEAD, will assume draft version"
-
-	version=draft
-fi
-
 # Create archive for each target runtime
+base="$(dirname "$0")"
 source="$(mktemp -d)"
 
 for runtime in $runtimes; do
-	archive="$artifact-$version-$runtime.zip"
+	archive="$artifact-$runtime.zip"
 
 	# Publish archive for requested runtime
 	echo >&2 "publishing $archive..."
